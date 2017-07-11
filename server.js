4f721aee42f2fad8cd181a14f9e80b6c69d612f1
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-07-11 17:18:23 -0400

    more attempts to troubleshoot heroku

--- server.js
+++ server.js
@@ -52,20 +52,20 @@ app.set("view engine", "handlebars");
 
 var db = process.env.MONGODB_URI ||"mongodb://heroku_7xxdzd98:8gemlpcea4d2ircinahjsk7e6b@ds153412.mlab.com:53412/heroku_7xxdzd98"
 
-// mongoose.connect(mongoConfig);
-//
-// // saves our mongoose connection to db
-// var db = mongoose.connection;
-//
-// // shows any mongoose errors
-// db.on("error", function(error) {
-//   	console.log("Mongoose Error: ", error);
-// });
-//
-// // logs a success message once logged in to the db through mongoose
-// db.once("open", function() {
-//   	console.log("Mongoose connection successful.");
-// });
+mongoose.connect(db);
+
+// saves our mongoose connection to db
+var db = mongoose.connection;
+
+// shows any mongoose errors
+db.on("error", function(error) {
+  	console.log("Mongoose Error: ", error);
+});
+
+// logs a success message once logged in to the db through mongoose
+db.once("open", function() {
+  	console.log("Mongoose connection successful.");
+});
 
 // handles routes
 app.use("/", routes);
@@ -74,89 +74,3 @@ app.use("/", routes);
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-//
-//
-// // dependencies
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var logger = require("morgan");
-// var dotenv = require("dotenv");
-//
-// // database ORM
-// var mongoose = require("mongoose");
-// // scraping tools
-// var request = require("request");
-// var cheerio = require("cheerio");
-//
-// // models needed
-// var Articles = require("./models/articles.js");
-// var Comments = require("./models/notes.js");
-//
-// // imports routes
-// var routes = require("./controllers/article_controller.js");
-//
-// // loads environment variables from .env file into process.env
-// dotenv.load();
-//
-// // sets mongoose to leverage Promises
-// mongoose.Promise = Promise;
-//
-// // sets port
-// var port = process.env.PORT || 3000;
-//
-// // initializes express
-// var app = express();
-//
-// // logs requests to the console
-// app.use(logger("dev"));
-//
-// // parses data
-// app.use(bodyParser.urlencoded({
-//   extended: false
-// }));
-//
-// // / makes public a static dir
-// app.use(express.static("public"));
-//
-// // sets Handlebars
-// var exphbs = require("express-handlebars");
-//
-// // sets default view engine to handlebars
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-//
-// app.set("view engine", "handlebars");
-//
-//
-// // COULDN''T GET THIS TO WORK ON HEROKU
-// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var db = process.env.MONGODB_URI || "mongodb://heroku_7xxdzd98:8gemlpcea4d2ircinahjsk7e6b@ds153412.mlab.com:53412/heroku_7xxdzd98";
-//
-// // var mongoConfig = process.env.MONGODB_URI || "mongodb://localhost/nprnewsscraper";
-// // mongoose.connect(mongoConfig);
-//
-//
-//  // routes
-// app.use("/", routes);
-//
-// // listens on port
-// app.listen(port, function() {
-// 	console.log("Listening on " + port);
-// });
